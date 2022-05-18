d354237c8b78d45b883bfc4ed415b446e93b1c34
karl-no <karl.publicidade@gmail.com>
2022-05-18 19:33:38 -0300

    Desafio 7 concluído

--- src/challenges.js
+++ src/challenges.js
@@ -52,8 +52,16 @@ function highestCount(numeros) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let distanceCat1 = mouse - cat1;
+  let distanceCat2 = mouse - cat2;
+  if(distanceCat1 > distanceCat2){
+    return 'cat2';
+  } else if (distanceCat2 > distanceCat1) {
+    return 'cat1';
+  } else if (distanceCat1 == distanceCat2){
+    return 'os gatos trombam e o rato foge';
+  }
 }
 
 // Desafio 8
